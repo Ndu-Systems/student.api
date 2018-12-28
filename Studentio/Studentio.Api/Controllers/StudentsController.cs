6ad18a46c8766ad574c848f5272d9f0592d583b4
freedomkhanyile <freedom.khanyile1@gmail.com>
2018-12-28 16:13:49 +0200

    Get all students works

---
+++ Studentio/Studentio.Api/Controllers/StudentsController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Studentio.Contracts.ILoggerService;
+using Studentio.Contracts.IRepositoryWrapper;
+using Studentio.Entities.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Studentio.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class StudentsController : Controller
+    {
+        private ILoggerManager _logger;
+        private IRepositoryWrapper _repoWrapper;
+
+        public StudentsController(ILoggerManager logger, IRepositoryWrapper repoWrapper)
+        {
+            _logger = logger;
+            _repoWrapper = repoWrapper;
+        }
+
+
+
+        // GET: api/students
+        [HttpGet]
+        public IActionResult Get()
+        {        
+            try
+            {
+                var students = _repoWrapper.Student.GetAllStudents();
+                if (students != null)
+                    _logger.LogInfo($"Students returned from database at : {DateTime.Now}");
+                return Ok(students);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Something went wrong inside Students/Get error: {ex.Message}");
+                return StatusCode(500, "internal server error");
+            }
+          
+        }
+
+        //// GET api/<controller>/5
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        //// POST api/<controller>
+        //[HttpPost]
+        //public void Post([FromBody]string value)
+        //{
+        //}
+
+        //// PUT api/<controller>/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
+
+        //// DELETE api/<controller>/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
